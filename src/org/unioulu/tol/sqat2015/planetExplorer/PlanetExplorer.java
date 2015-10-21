2c46edc05c48df9ccd1a48e1865b962080f03f14
somename <someemail>
2015-10-21 16:29:59 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -33,7 +33,7 @@ public class PlanetExplorer {
 //			}
 //		}
 		if(obstacles.length() > 4){
-			this.obstacleList = new Obstacle(0,1);
+			this.obstacleList = new Obstacle(obstacles.charAt(1),1);
 		}else{
 			this.obstacleList = new Obstacle(-10,-10);
 		}
