1ad0a0e34545105b65698ce04ebb1fda3199600d
somename <someemail>
2015-10-21 14:52:34 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -6,6 +6,9 @@ package org.unioulu.tol.sqat2015.planetExplorer;
 public class PlanetExplorer {
 	int gridSizeX;
 	int gridSizeY;
+	int positionX;
+	int positionY;
+	char facing;
 	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
@@ -14,6 +17,11 @@ public class PlanetExplorer {
 		Example use:
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
 	 */
+		this.gridSizeX = x;
+		this.gridSizeY = y;
+		this.positionX = 0;
+		this.positionY = 0;
+		this.facing = 'N';
 	}
 	
 	public String executeCommand(String command){
@@ -27,6 +35,9 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		for(char c : command.toCharArray()){
+			
+		}
 		if(command == "f"){
 			return "(0,1,N)";
 		}
